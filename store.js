5d5460f69b06805c438879dea714b4034d031319
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-04-28 23:33:23 +0530

    todo

---
+++ store.js
@@ -0,0 +1,20 @@
+const info = document.getElementById("input").value
+
+
+    const initState = {
+        todo: []
+    }
+
+const reducer = (store, action)=>{
+    if (action.type === "ADD_TODO") {
+        return {...store, todo:[...store.todo , action.payload]}
+    }
+    return store
+}
+const store = Redux.createStore(reducer, initState)
+const state = store.getState()
+
+document.getElementById("btn").addEventListener("click", function () {
+    store.dispatch({ type: "ADD_TODO", payload: {title: "partha", status:false} })
+    console.log(state)
+})
